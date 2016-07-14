31baefc7d76f972a510d9620ff285d561cf019b4
abiramiarul <abi.arul29@gmail.com>
2016-07-14 18:43:53 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-int main(){
-    int year;
-
-    printf("Enter any year: ");
-    scanf("%d",&year);
-
-    if(((year%4==0)&&(year%100!=0))||(year%400==0))
-         printf("%d is a leap year",year);
-    else
-         printf("%d is not a leap year",year);
-
-    return 0;
+#include<conio.h>
+void main()
+{
+int year;
+clrscr();
+scanf("%d",&year);
+if(year%4==0)
+printf("Leap year");
+else 
+printf("not a leap year");
+getch();
 }
