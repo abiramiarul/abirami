82552d500447e71a60336453f1cbdf4d1f54aa63
abiramiarul <abi.arul29@gmail.com>
2016-07-15 09:28:20 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the no");
+scanf("%d%d",&a,&b);
+int *p,*p1;
+p=&a;
+P1=&b;
+c=*p+*p1;
+printf("addition of two no%d",c);
+}
+
